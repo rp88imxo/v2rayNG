6a2c1969fead2a861491b923afe0a9e9bc70d634
rp88imxo <rp88imxo@gmail.com>
2025-11-07 21:22:41 +0300

    initial setup

--- V2rayNG/app/src/main/java/com/v2ray/ang/util/HttpUtil.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/util/HttpUtil.kt
@@ -136,7 +136,7 @@ object HttpUtil {
         while (redirects++ < maxRedirects) {
             if (currentUrl == null) continue
             val conn = createProxyConnection(currentUrl, httpPort, timeout, timeout) ?: continue
-            conn.setRequestProperty("User-agent", "v2rayNG/${BuildConfig.VERSION_NAME}")
+            conn.setRequestProperty("User-agent", "Flint/${BuildConfig.VERSION_NAME}")
             conn.connect()
 
             val responseCode = conn.responseCode
